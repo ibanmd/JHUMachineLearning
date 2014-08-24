ba9521f9aed47d856705a4c879af5f6973f91a0e
ibanmd <ibanmd@gmail.com>
2014-08-24 04:26:39 -0400

    Update

--- MLcourseProjectCleaning.R
+++ MLcourseProjectCleaning.R
@@ -78,3 +78,25 @@ rm(numOfNAs)
 
 save(train, file="traincleaned.Rda")
 save(test, file="testcleaned.Rda")
+
+## Function that brings back a skinny data frame with the correlation for every two variables
+## Instead I ended up using preprocessing to get down to the 30 variables but this would be a way to 
+## manually eliminate some.
+pairwiseCor <- function(dataframe){
+  pairs <- combn(names(dataframe), 2, simplify=FALSE)
+  df <- data.frame(Vairable1=rep(0,length(pairs)), Variable2=rep(0,length(pairs)), 
+                   AbsCor=rep(0,length(pairs)), Cor=rep(0,length(pairs)))
+  for(i in 1:length(pairs)){
+    df[i,1] <- pairs[[i]][1]
+    df[i,2] <- pairs[[i]][2]
+    df[i,3] <- round(abs(cor(dataframe[,pairs[[i]][1]], dataframe[,pairs[[i]][2]])),4)
+    df[i,4] <- round(cor(dataframe[,pairs[[i]][1]], dataframe[,pairs[[i]][2]]),4)
+  }
+  pairwiseCorDF <- df
+  pairwiseCorDF <- pairwiseCorDF[order(pairwiseCorDF$AbsCor, decreasing=TRUE),]
+  row.names(pairwiseCorDF) <- 1:length(pairs)
+  #pairwiseCorDF <<- pairwiseCorDF
+  #pairwiseCorDF
+}
+
+
---
+++ submissionScript.R
@@ -0,0 +1,13 @@
+## Given to us by the professors to aid in submitting our 20 predictions
+
+answers <- predict(modelRF, testPC)
+
+pml_write_files = function(x){
+  n = length(x)
+  for(i in 1:n){
+    filename = paste0("problem_id_",i,".txt")
+    write.table(x[i],file=filename,quote=FALSE,row.names=FALSE,col.names=FALSE)
+  }
+}
+
+pml_write_files(answers)
